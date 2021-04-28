e1daf8bd4b1d0544f9280c4fc4071829d44378f7
Gangaraju Chaganti <gangaraju.chaganti@syncron.com>
2021-04-28 12:48:07 +0530

    Add bucket arn to output

--- outputs.tf
+++ outputs.tf
@@ -13,3 +13,11 @@ output "s3_domain_name" {
 output "website_address" {
   value = var.domain_name
 }
+
+output "s3_bucket_arn" {
+  value = aws_s3_bucket.s3_bucket.arn
+}
+
+output "s3_bucket_name" {
+  value = aws_s3_bucket.s3_bucket.id
+}
