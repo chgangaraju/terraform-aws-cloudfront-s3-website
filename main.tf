85682deaf26e9997dc51cc172f4eeaa1ba84dd28
Kevin Lyda <kevin@lyda.ie>
2022-04-24 18:35:43 +0100

    Address s3 versioning changes
    The aws provider has changed how s3 bucket versioning is handled.

--- main.tf
+++ main.tf
@@ -44,11 +44,15 @@ data "aws_iam_policy_document" "s3_bucket_policy" {
 resource "aws_s3_bucket" "s3_bucket" {
   bucket = var.domain_name
   acl    = "private"
-  versioning {
-    enabled = true
-  }
   policy = data.aws_iam_policy_document.s3_bucket_policy.json
   tags   = var.tags
+}
+
+resource "aws_s3_bucket_versioning" "s3_bucket" {
+  bucket = var.domain_name
+  versioning_configuration {
+    status = "Enabled"
+  }
 }
 
 resource "aws_s3_bucket_object" "object" {
